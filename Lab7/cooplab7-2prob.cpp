ca1169b97196ee7cca08f0d0a5c593670ad42bfd
Kayw Lee <kaywoong@gmail.com>
2019-10-21 00:38:55 -0700

    cooplab2

--- Lab7/cooplab7-2prob.cpp
+++ Lab7/cooplab7-2prob.cpp
@@ -40,54 +40,15 @@ int		main()
 
 void		findMaxSum(int result[], int num[][5], int rows, int cols)
 {
-	int sumrow = 0, sum, idx;
-	for(int i=0; i<rows; i++) {
-		sum = 0;
-		for(int	j=0; j<cols; j++){
-			cout << setw(5) << num[i][j];
-			sum += num[i][j];
-		}
-		cout << "\tThe sum of row is :" << setw(5) << sum << endl;
-		if (i==0) {
-			sumrow = sum;
-			idx = i;
-		}
-		else if (sumrow < sum){
-			sumrow = sum;
-			idx = i;
-		}
-	}
-	for(int i=0; i<cols; i++)
-		result[i] = num[idx][i];
+	
 }
 
 void		findMaxElm(int result[], int num[][5], int rows, int cols)
 {
-	int 	max;
-	for(int i=0; i<rows; i++) {
-		max = 0;
-		for(int	j=0; j<cols; j++){
-			if (j==0) 
-				max = num[i][j];
-			else if (max < num[i][j])
-				max = num[i][j];
-		}
-		result[i] = max;
-	}
+	
 }
 
 void		findMaxVal(int result[], int num[][5], int rows, int cols)
 {
-	int  max, idx;
-	max = num[0][0];
-	for(int i=0; i<rows; i++) {
-		for(int	j=0; j<cols; j++){
-			if (max < num[i][j]){
-				max = num[i][j];
-				idx = i;
-			}
-		}
-	}
-	for(int i=0; i<cols; i++)
-		result[i] = num[idx][i];
+	
 }
