dc2fa2d43ea481e4041599e20e103adf462f5c41
Kayw Lee <kaywoong@gmail.com>
2019-10-24 22:25:01 -0700

    lab6-10

--- Lab6/lab6-10.cpp
+++ Lab6/lab6-10.cpp
@@ -41,3 +41,52 @@ int	main()
 		}
 	} while ( num != -1);
 }
+void	getinput(int &num)
+{
+	do {
+	cout << "Enter your input ( 2, 3, or 4 )\n";
+	cin >> num; 
+	} while ( num != 2 && num != 3 && num != 4 && num != -1);
+}
+void	getinput(int &num1, int &num2)
+{
+	cout << "Enter your input ( 2 integers )\n";
+	cin >> num1 >> num2 ;
+}
+void	getinput(int &num1, int &num2, int &num3)
+{
+	cout << "Enter your input ( 3 integers )\n";
+	cin >> num1 >> num2 >> num3;
+}
+void	getinput(int &num1, int &num2, int &num3, int &num4)
+{
+	cout << "Enter your input ( 4 integers )\n";
+	cin >> num1 >> num2 >> num3 >> num4;
+}
+void	swap(int &i, int &j)
+{
+	int 	tmp;
+	tmp = i;
+	i = j;
+	j = tmp;
+}
+void	swap(int &num1, int &num2, int &num3)
+{
+	if ( num1 >  num2 )
+		swap(num1, num2);
+	if ( num1 > num3 )
+		swap(num1, num3);
+	// at this moment, the num1 is min.
+	
+	if ( num2 > num3)
+		swap(num2, num3);
+			
+}
+void	swap(int &num1, int &num2, int &num3, int &num4)
+{
+	swap(num1, num2);
+	swap(num2, num3);
+	swap(num3, num4);
+}
+
+
