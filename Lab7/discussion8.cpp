f87ce4c0e8e8216240b9a955afab6f90e325a6fd
Kayw Lee <kwooler@gmail.com>
2019-11-13 10:09:04 -0800

    discussion-8

---
+++ Lab7/discussion8.cpp
@@ -0,0 +1,23 @@
+// Character 2D Array
+
+#include	<iostream>
+using namespace std;
+
+int	main()
+{
+	char 	cstrarray[5][10] = { "C++", 
+								 "Python",
+								 "Java",
+								 "JSP",
+								 "Html"};
+	for(int	i=0; i<5; i++)
+		cout << cstrarray[i] << endl;
+	
+
+	for(int	i=0; i<5; i++){
+		for(int j=0;cstrarray[i][j] != '\0'; j++)
+		//for(int j=0;j<10; j++)   // Wrong. There are invalid value over the null value.
+			cout << cstrarray[i][j];
+		cout << endl;
+	}
+}
