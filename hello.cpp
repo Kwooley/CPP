67fa13f49b4dec339ea4b1bbead1b142fe8eb178
Kyu Lee <kwooler@gmail.com>
2023-07-16 19:45:15 -0700

    CPP

--- hello.cpp
+++ hello.cpp
@@ -7,10 +7,13 @@ int main()
     cout << "hello World\n";
 
     // "add comment"
-    for (int j = 0; j < 10; j++)
+    for (;;)
     {
-        cout << "test\n";
-        cout << "test2\n";
-        cout << "test3\n";
+        for (int j = 0; j < 10; j++)
+        {
+            cout << "test\n";
+            cout << "test2\n";
+            cout << "test3\n";
+        }
     }
 }
