ee846ab27226c51f18d8a91108189109ce523ee6
Kyu Lee <kwooler@gmail.com>
2023-07-16 19:42:18 -0700

    CPP

--- hello.cpp
+++ hello.cpp
@@ -5,4 +5,7 @@ int main()
 {
     cout << "Add a line\n";
     cout << "hello World\n";
+
+    for
+        i in range(len(numbers)) : diff = abs(average - numbers[i]) if diff > max_diff : max_diff = numbers[i]
 }
